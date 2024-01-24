66239695dd29a6f767b9b4598ffd1b9f4a26b346
Hyusein Osmanov <osmanovhyus@gmail.com>
2024-01-24 20:09:39 +0200

    began implementing account functionalities

--- main/settings.py
+++ main/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'chatting.apps.ChattingConfig',
+    'accounts.apps.AccountsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
