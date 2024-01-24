8b8b855ccbd134785232d860faf708ebaa14341c
Hyusein Osmanov <osmanovhyus@gmail.com>
2024-01-24 21:33:21 +0200

    began implementing registration

--- accounts/views.py
+++ accounts/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
+from django.contrib.auth.models import User
 
 # Create your views here.
 
 def register(request):
-    return render(request, 'register.html')
+    if request.method == "GET":
+        return render(request, 'register.html')
+    else:
+        return render(request, 'index.html')
