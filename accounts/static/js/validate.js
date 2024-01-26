2f6e34ccd4bc5da94381d760a671f6f1a8fdbe62
Hyusein Osmanov <osmanovhyus@gmail.com>
2024-01-26 23:07:08 +0200

    finished register page design, implemented some js information code

---
+++ accounts/static/js/validate.js
@@ -0,0 +1,20 @@
+let lineDivs = document.getElementsByClassName('line-div');
+let usernameInput = document.getElementById('username');
+
+usernameInput.addEventListener('focus', function (event) {
+    console.log('focused');
+    let message = document.createElement('p');
+    message.classList.add('lead');
+    message.classList.add('message');
+    message.style.color = 'white';
+    message.style.paddingLeft = '1em';
+
+    message.innerHTML = 'Usernames should include alphanumerical lowercase characters, underscores (_) and/or hyphens(-).'
+    lineDivs[1].insertAdjacentElement('beforebegin', message);
+});
+
+usernameInput.addEventListener('blur', function (event) {
+    let formChildren = document.getElementsByClassName('inputs-div')[0];
+    let element = formChildren.querySelector('p.lead.message');
+    formChildren.removeChild(element);
+});
