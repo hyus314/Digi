d6cee05a64afd155f054a526ff94e361ff4a9224
Hyusein Osmanov <osmanovhyus@gmail.com>
2024-01-31 18:18:40 +0200

    linked front-end register form with server-side logic

--- accounts/views.py
+++ accounts/views.py
@@ -7,5 +7,18 @@ from django.views.decorators.csrf import csrf_protect
 def register(request):
     if request.method == "GET":
         return render(request, 'register.html')
-    elif request.method == "POST":
-        return redirect('index')
+    
+    username = request.POST.get('username')
+    first_name = request.POST.get('first')
+    last_name = request.POST.get('last')
+    email = request.POST.get('email')
+    password = request.POST.get('password')
+    confirmation = request.POST.get('confirmation')
+
+    print(username)
+    print(first_name)
+    print(last_name)
+    print(email)
+    print(password)
+    print(confirmation)
+    return redirect('index')
