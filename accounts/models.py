6a5aee3bb6c2c90e518a2c0032039ed18f15676f
Hyusein Osmanov <osmanovhyus@gmail.com>
2024-02-21 17:07:48 +0200

    Implementing the token modal in the profile page

--- accounts/models.py
+++ accounts/models.py
@@ -1,11 +1,11 @@
 from django.db import models
 from django.contrib.auth.models import User
 
-class tokens(models.Model):
+class Tokens(models.Model):
     id = models.AutoField(primary_key=True)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     token = models.CharField(max_length=9)
 
     def __str__(self):
         return f"Token: {self.token}"
-# Create your models here.
+
