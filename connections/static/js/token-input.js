7f0507c177a844c40e10a138b6244adb02a91c98
Hyusein Osmanov <osmanovhyus@gmail.com>
2024-03-03 20:51:38 +0200

    Implementing front-end validation for connect page

--- connections/static/js/token-input.js
+++ connections/static/js/token-input.js
@@ -1,8 +1,23 @@
 let input = document.querySelector('#input');
+const pattern = /^DG[A-Za-z0-9]{6}X$/;
 
-input.addEventListener('input', async function() {
+input.addEventListener('input', async function () {
     const value = input.value
+    slice(value);
+    let tokenDiv = document.querySelector('.token-result');
+    if (pattern.test(value)) {
+        tokenDiv.style.backgroundColor = 'green';
+    } else {
+        tokenDiv.style.backgroundColor = 'black';
+    }
     // console.log(value);
     let result = document.querySelector('.token-result>p');
-    result.innerHTML = value; 
+    result.innerHTML = value;
 });
+
+function slice(value) {
+    if (value.length > 9) {
+        value = value.slice(0, 9);
+        input.value = value;
+    }
+}
