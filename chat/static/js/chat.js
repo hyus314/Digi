ffe78a56013611916eba6bf82463db1259534ec4
Hyusein Osmanov <osmanovhyus@gmail.com>
2024-04-09 20:35:42 +0300

    included chat web socket js code and connection id in dom tree

--- chat/static/js/chat.js
+++ chat/static/js/chat.js
@@ -1,6 +1,13 @@
 let sendButton = document.getElementById('send');
-
+const connectionId = document.getElementById('connectionId').value;
+console.log(connectionId);
 sendButton.addEventListener('click', (e) => {
     e.preventDefault();
     console.log('clicked');
 });
+
+const chatSocket = new WebSocket(
+    'ws://'
+    + window.location.host
+    + '/ws/chat/'
+);
--- chat/views.py
+++ chat/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
 
 def chat(request):
-    return render(request, 'chat.html')
+    chat_id = request.GET.get('id')
+    return render(request, 'chat.html', {'chat_id': chat_id})
